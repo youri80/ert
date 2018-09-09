1405ae7ed8807ba82473788ad40d75c1104db0d6
Christian Lange <christian@youri.de>
2018-09-09 21:33:49 +0200

    button and sheet management

--- Ert.Core/Services/EaCreationService.cs
+++ Ert.Core/Services/EaCreationService.cs
@@ -1,10 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OfficeOpenXml;
+
 namespace Ert.Core.Services
 {
     public class EaCreationService
     {
+
         public EaCreationService()
         {
+
+
+        }
+
+        Dictionary<string, IEnumerable<string>> ButtonRemoveList = new Dictionary<string, IEnumerable<string>>();
+
+        Dictionary<string, IEnumerable<string>> WorksheetHiddeList = new Dictionary<string, IEnumerable<string>>();
+
+        /// <summary>
+        /// Abfrage aus der Datenbank? 
+        /// </summary>
+        private void LoadButtonRemoveList() {
+
+        }
+
+        /// <summary>
+        /// Abfrage aus der Datenbank
+        /// </summary>
+        private void LoadSheetHiddeList(){
+
+        }
+
+        private void LoadTemplate(){
+
+        }
+
+        public void RemoveButtons(IEnumerable<string> buttonNames, ExcelWorksheet ws)
+        {
+                foreach (var b in buttonNames)
+                {
+
+                    try
+                    {
+                        ws.Drawings.Remove(b);
+                    }
+                    catch (Exception exp)
+                    {
+
+                    }
+
+                }
+        }
+
+        public void HiddeSheets(IEnumerable<string> sheetNames, ExcelWorkbook workbook)
+        {
+            foreach (var name in sheetNames){
+               
+                workbook.Worksheets[name].Hidden = eWorkSheetHidden.Hidden;
+            }
+        }
+
+        public void CreateEaFromTemplate(){
+
+            using (var p = new ExcelPackage()){
+
+                    //A workbook must have at least on cell, so lets add one... 
+                    var ws = p.Workbook.Worksheets.Add("MySheet");
+                //To set values in the spreadsheet use the Cells indexer.
+                ws.Cells["A1"].Value = "This is cell A1";
+                //Save the new workbook. We haven't specified the filename so use the Save as method.
+
+                p.SaveAs(new FileInfo(@"c:\workbooks\myworkbook.xlsx"));
+            }
         }
     }
 }
--- Ert.Core/Services/FileService.cs
+++ Ert.Core/Services/FileService.cs
@@ -7,27 +7,28 @@ namespace Ert.Core.Services
     {
         public string DataDir { get;  set; }
 
-
-
         public void SaveEa(string filename, byte[] content)
         {
             File.WriteAllBytes(Path.Combine(DataDir, filename), content);
         }
 
-        public void GetEa(string mfid, string cls, DateTime navdate){
+        public void GetFullPath(string mfid, string cls, DateTime navdate){
             
         }
 
-        public string FilePath(){
-             throw new NotImplementedException();
+        public void GetFullPath (int calcid){
+
         }
 
-        public string CreateFullFilenameEa(string mfid, string cls, DateTime navdate, bool isZwa = false, string investor = null){
-            var filename = $"{mfid}_{cls}_{navdate.ToString("yyyyMMdd")}";
-
+        public string CreateFilenameEa(string mfid, string cls, DateTime navdate, bool isZwa = false, string investor = null){
+            var filename =  $"{mfid}_{cls}_{navdate.ToString("yyyyMMdd")}";
+            
             if (investor != null) {
                 filename = $"{filename}_{investor}";
             }
+
+            //@todo Feststellung Spezialfonds
+
             return Path.Combine(isZwa?"Zwa": String.Empty, filename);
 
         }
